1cdd5d066eba1d52d3a43693176586abef5f153b
walterjrr <jrwalter731@gmail.com>
2024-12-06 00:48:49 -0300

    sistema de rota post e recebimento dos dados do formulario

--- server.js
+++ server.js
@@ -3,20 +3,42 @@ const app = express();
 const path = require("path");
 const port = 3000
 const data = require('./data');
+let users = [];
 
-//app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")))
+const bodyParser = require('body-parser');
 
-app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
     res.render('index', data)
 })
 
 app.get('/submited', (req, res) => {
-    res.render('submited', )
+    res.render('submited', {users} )
+})
+
+app.post('/submited', (req, res) => {
+    const { name, login, password } = req.body;
+    console.log('chegou ate aqui', req.body)
+
+    if (!name || !login || !password) {
+        return res.status(400).send('Todos os campos são obrigatorios')
+    }
+
+    users.push({ name, login, password });
+
+    res.redirect('/users');
+
+})
+
+app.get('/users', (req, res) => {
+    res.render('users', {users})
 })
 
 app.listen(port, () => {
-    console.log(`server runing in localhost , ${port}`)
+    console.log(`server runing at localhost , ${port}`)
 })
--- views/script.js
+++
@@ -1,5 +0,0 @@
-const button = document.getElementById("ballon");
-console.log(button)
-button.addEventListener('click', () => {
-    alert("click")
-})
