ec60107e233bc212008ea03c4d812c5b1a4cd2d1
walterjrr <jrwalter731@gmail.com>
2024-12-12 09:29:53 -0300

    atualizar dados com o mongodb

--- models/user.js
+++ models/user.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const mongoURI = 'mongodb://localhost:27017/formejs';
 
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('Conexão com o mongoDb estabelecida com sucesso!')).catch(err => console.log('Erro ao conectar ao mongoDb', err));
+mongoose.connect(mongoURI).then(() => console.log('Conexão com o mongoDb estabelecida com sucesso!')).catch(err => console.log('Erro ao conectar ao mongoDb', err));
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const path = require("path");
 const port = 3000
@@ -7,7 +7,7 @@ const data = require('./data');
 const user = require('./models/user');
 const User = require("./models/user");
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.set('view engine', 'ejs');
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/submited', (req, res) => {
-    res.render('submited', {users} )
+    res.render('submited', { users })
 })
 
 app.post('/submited', async (req, res) => {
@@ -26,14 +26,14 @@ app.post('/submited', async (req, res) => {
     console.log('chegou ate aqui', req.body)
 
     try {
-        const newUser = new User({name, login, password });
+        const newUser = new User({ name, login, password });
         await newUser.save();
     } catch (err) {
         console.error('erro ao salvar o usuario:', err);
         res.status(500).send('erro ao salvar o usuario');
     }
     res.redirect('/users');
-    
+
 })
 
 app.get('/users', async (req, res) => {
@@ -47,49 +47,68 @@ app.get('/users', async (req, res) => {
     }
 })
 
-app.post('/delete', (req, res) => {
+app.post('/delete', async (req, res) => {
     const { login } = req.body;
-    
+
+    try {
+        await User.deleteOne({ login });
+    } catch (err) {
+        ('Erro ao deletar os usuarios', err);
+        res.status(500).send('erro ao deletar os usuarios');
+    }
+
     if (!login) {
         return res.status(400).send("login do Usuario é obrigatorio para deletar")
     }
-    
-    users = users.filter(user => user.login !== login);
+
     res.redirect("/users");
 })
 
 
-app.get('/edit/:login', (req, res) => {
-    const {login} = req.params;
-    
-    const user = users.find(user => user.login === login)
-    
-    if(!user) {
+app.get('/edit/:login', async (req, res) => {
+    const { login } = req.params;
+    console.log(req.params)
+
+
+    try {
+        const user = await User.findOne({login})
+        if (!user) {
         return res.status(404).send("Usuario não encontrado")
     }
-    
-    res.render('edit', { 
-        title: "Editar Usuário", 
-        message: "Atualize as informações do usuário", 
-        name: "Nome", 
-        password: "Senha", 
-        user 
+        
+        res.render('edit', {
+        title: "Editar Usuário",
+        message: "Atualize as informações do usuário",
+        name: "Nome",
+        password: "Senha",
+        user
     });
+    } catch (err) {
+        res.status(500).send("Erro ao buscar o usuário: " + err.message);
+    }
+    
 })
 
-app.post("/edit/:login", (req, res) => {
+app.post("/edit/:login", async (req, res) => {
     const { login } = req.params;
-    const {name, newLogin, password } = req.body;
-    
+    const { name, password, newLogin } = req.body;
+
     console.log(req.params)
-    const userIndex = users.findIndex(user => user.login === login)
-    
-    if (userIndex === -1) {
-        return res.status(404).send("Usuario não encontrado")
+
+    try {
+        const updateLogin = await User.findOneAndUpdate(
+            {login},
+            {name, password, 
+            login: newLogin || login},
+            {new: true}
+        )
+
+        if (!updateLogin) {
+            return res.status(404).send("Usuário não encontrado");
+        }
+    } catch (err) {
+        res.status(500).send("Erro ao atualizar o usuário: " + err.message);
     }
-    
-    users[userIndex] = {name, login: newLogin || login, password};
-    console.log(req.params)
     res.redirect("/users")
 })
 
