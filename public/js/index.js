07fe9bff9e473b46cb1dc68bd0bacb30d9d96933
walterjrr <jrwalter731@gmail.com>
2024-12-25 14:46:40 -0300

    validação e força de senha

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,39 @@
 const form = document.getElementById('registrationForm');
 const loginInput = document.getElementById('login');
+const passwordInput = document.getElementById('password')
+
+let passwordok = false
+
+const strength = (() => {
+    let passwordValue = document.getElementById('password').value;
+    let strengthpassword = ["fraca", "média", "forte"]
+
+    if (passwordValue.length < 4) {
+        document.getElementById('showpassword').innerHTML = strengthpassword[0],
+        passwordok = false
+    }
+    if (/[A-Z]/.test(passwordValue)) {
+        document.getElementById('showpassword').innerHTML = strengthpassword[1],
+        passwordok = true
+    }
+    if (/[^a-zA-Z0-9]/g.test(passwordValue)) {
+        document.getElementById('showpassword').innerHTML = strengthpassword[2],
+        passwordok = true
+    }
+
+})
+
+document.getElementById('password').addEventListener('keydown', strength);
+
 
 form.addEventListener('submit', async (e) => {
+    if(!passwordok) {
+        e.preventDefault();
+        alert('A senha deve conter letra maiuscula e caracteres especiais')
+        return;
+    }
+
+
     e.preventDefault();
 
     const formData = new FormData(form);
@@ -15,10 +47,7 @@ form.addEventListener('submit', async (e) => {
         });
 
         if (!response.ok) {
-            
             const result = await response.json();
-
-            
             if (result.field === 'login') {
                 loginInput.classList.add('is-invalid');
                 const invalidFeedback = loginInput.nextElementSibling;
@@ -26,8 +55,6 @@ form.addEventListener('submit', async (e) => {
             }
             return;
         }
-
-       
         window.location.href = '/users';
     } catch (err) {
         console.error('Erro na requisição:', err);
